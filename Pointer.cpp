929974bcf8161f569b6001d1f54acd94e76ad810
Rajeep Subedi <rajeep.subedi@trojans.dsu.edu>
2017-05-14 10:27:47 -0500

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int var = 1;
+int main()
+{
+   int num = 10;
+   printf("\n");
+   printf("The Value of var is: %d\n", num);
+   printf("The Memory Address of var is: %p\n", &num);
+   printf("\n");
+   return 0;
+}
